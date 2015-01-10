cc41391b471760f0310b64c90bc25cab6795ede6
xala3pa <salazar3pa@gmail.com>
2015-01-10 16:17:57 +0100

    url routing

---
+++ url-routing/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", HomeHandler)
+
+	// Posts collection
+	posts := r.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+
+	// Posts singular
+	post := r.PathPrefix("/posts/{id}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("GET").HandlerFunc(PostShowHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Home")
+}
+
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post update")
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post delete")
+}
+
+func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post edit")
+}
