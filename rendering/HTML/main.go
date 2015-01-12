1946b3711741246e13ff5914feb8e2b7d9492711
xala3pa <salazar3pa@gmail.com>
2015-01-12 23:20:28 +0100

    HTML rendering

--- rendering/HTML/main.go
+++ rendering/HTML/main.go
@@ -12,6 +12,8 @@ type App struct {
 	Description string
 }
 
+var tmpl = template.Must(template.ParseFiles(path.Join("templates", "index.html")))
+
 func main() {
 	http.HandleFunc("/", ShowApps)
 	http.ListenAndServe(":8080", nil)
@@ -19,13 +21,6 @@ func main() {
 
 func ShowApps(w http.ResponseWriter, r *http.Request) {
 	app := App{"Gymgo", "Álvaro Salazar", "Track your fitness goals"}
-
-	fp := path.Join("templates", "index.html")
-	tmpl, err := template.ParseFiles(fp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
 	if err := tmpl.Execute(w, app); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
